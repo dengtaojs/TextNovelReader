815cef2be2869155e656a587046c7cdef42d09c6
DengTao <dengtaowx@outlook.com>
2025-12-23 18:54:24 +0800

    Add project files.

---
+++ TextNovelReader/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace TextNovelReader;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object? sender, EventArgs e)
+    {
+        //SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
