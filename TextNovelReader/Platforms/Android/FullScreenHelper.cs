83ff52011ded2ecd89617ecd41011c09d423e0db
fhhjfjjhhg <fhhjfjjhhg@noreply.gitcode.com>
2025-12-30 14:22:15 +0800

    加入全屏阅读

---
+++ TextNovelReader/Platforms/Android/FullScreenHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Android.App;
+using Android.Views;
+
+namespace TextNovelReader.Platforms.Android;
+
+#pragma warning disable CA1416
+
+public static class FullScreenHelper
+{
+    public static void EnterFullScreen(Activity activity)
+    {
+        if (activity.Window == null)
+            return;
+
+        activity.Window.SetFlags(
+            WindowManagerFlags.Fullscreen, 
+            WindowManagerFlags.Fullscreen);
+
+
+        //activity.Window.DecorView.WindowInsetsController?.Hide(
+        //    WindowInsets.Type.Ime() |
+        //    WindowInsets.Type.NavigationBars()
+        //    );
+    }
+
+    public static void ExitFullScreen(Activity activity)
+    {
+        if (activity.Window == null)
+            return;
+
+        activity.Window.ClearFlags(WindowManagerFlags.Fullscreen);
+        //activity.Window.DecorView.WindowInsetsController?.Show(
+        //    WindowInsets.Type.Ime() |
+        //    WindowInsets.Type.NavigationBars());
+    }
+}
+
+#pragma warning restore CA1416
--- TextNovelReader/Views/ChapterDetailPage.xaml.cs
+++ TextNovelReader/Views/ChapterDetailPage.xaml.cs
@@ -26,13 +26,24 @@ public partial class ChapterDetailPage : ContentPage, IBackButtonHandler
     protected override void OnAppearing()
     {
         base.OnAppearing();
-        Shell.SetTabBarIsVisible(this, false); 
+        Shell.SetTabBarIsVisible(this, false);
+        var activity = Platform.CurrentActivity; 
+        if(activity != null)
+        {
+            Platforms.Android.FullScreenHelper.EnterFullScreen(activity); 
+        }
     }
 
     protected override void OnDisappearing()
     {
         base.OnDisappearing();
-        Shell.SetTabBarIsVisible(this, true); 
+        Shell.SetTabBarIsVisible(this, true);
+
+        var activity = Platform.CurrentActivity; 
+        if(activity != null)
+        {
+            Platforms.Android.FullScreenHelper.ExitFullScreen(activity); 
+        }
     }
 
 
